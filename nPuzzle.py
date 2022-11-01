e6cf22de951000004eeb3c04072b58442eb919be
raymondwzeng <rzeng3@gmail.com>
2022-11-01 11:18:26 -0700

    More fixes - linebreak on user input, return on impossible, depth tracking

--- nPuzzle.py
+++ nPuzzle.py
@@ -15,10 +15,9 @@ easy = [[1, 2, 0],
 doable = [[0, 1, 2],
           [4, 5, 3],
           [7, 8, 6]]
-# oh_boy = [[8, 7, 1],
-#           [6, 0, 2],
-#           [5, 4, 3]]
-oh_boy = [[0, 7, 2],[4, 6, 1],[3, 5, 8]]
+oh_boy = [[8, 7, 1],
+          [6, 0, 2],
+          [5, 4, 3]]
 
 def main():
     puzzle_mode = input("Welcome to an 8-Puzzle Solver. Type '1' to use a default puzzle, or '2' to create your own." + '\n')
@@ -38,8 +37,8 @@ def main():
             puzzle_row_one[i] = int(puzzle_row_one[i])
             puzzle_row_two[i] = int(puzzle_row_two[i])
             puzzle_row_three[i] = int(puzzle_row_three[i])
-            user_puzzle = [puzzle_row_one, puzzle_row_two, puzzle_row_three]
-            select_and_init_algorithm(user_puzzle)
+        user_puzzle = [puzzle_row_one, puzzle_row_two, puzzle_row_three]
+        select_and_init_algorithm(user_puzzle)
     return
 
 def init_default_puzzle_mode():
@@ -60,9 +59,6 @@ def init_default_puzzle_mode():
     if selected_difficulty == "4":
         print("Difficulty of 'Oh Boy' selected.")
         return oh_boy
-    # if selected_difficulty == "5":
-        # print("Difficulty of 'Impossible' selected.")
-        # return impossible
 
 def print_puzzle(puzzle):
     for i in range(0, 3):
@@ -70,11 +66,14 @@ def print_puzzle(puzzle):
     print('\n')
 
 def print_solution(node):
+    depth = 0
     current_node = node
     while current_node.parent != None:
         print_puzzle(current_node.board)
         current_node = current_node.parent
+        depth += 1
     print_puzzle(current_node.board)
+    print(f"Depth: {depth}")
 
 def select_and_init_algorithm(puzzle):
     algorithm=input("Select algorithm. (1) for Uniform Cost Search, (2) for the Misplaced Tile Heuristic, "
@@ -96,16 +95,12 @@ def uniform_cost_search(puzzle, heuristic):
     max_queue_size=0
     repeated_states.add(starting_node.tostring())
 
-    stack_to_print=[] # the board states are stored in a stack
-    
     while len(working_queue) > 0:
         max_queue_size=max(len(working_queue), max_queue_size)
         # the node from the queue being considered/checked
         node_from_queue=min_heap_esque_queue.heappop(working_queue)
         repeated_states.add(node_from_queue.tostring())
         if node_from_queue.solved():  # check if the current state of the board is the solution
-            # while len(stack_to_print) > 0:  # the stack of nodes for the traceback
-                # print_puzzle(stack_to_print.pop())
             print_solution(node_from_queue)
             print("Number of nodes expanded:", num_nodes_expanded)
             print("Max queue size:", max_queue_size)
@@ -161,9 +156,11 @@ def uniform_cost_search(puzzle, heuristic):
                     min_heap_esque_queue.heappush(working_queue, newState)
                     repeated_states.add(newState.tostring())
                     num_nodes_expanded += 1
-        # print(f"Next state: with depth {working_queue[0].depth} and heuristic {working_queue[0].heuristic}\n")
-        # print(f"Board: {working_queue[0].board}\n")
-        stack_to_print.append(node_from_queue.board)
+        if len(working_queue) > 0: #Safeguard against out-of-bounds error if no solution exists.
+            print(f"Next state: with depth {working_queue[0].depth} and heuristic {working_queue[0].heuristic}\n")
+            print(f"Board: {working_queue[0].board}\n")
+
+    print("Unable to solve the puzzle - all states have been exhausted.")
 
 #Defining the actual main function
 if __name__ == "__main__":
